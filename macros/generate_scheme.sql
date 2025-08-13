5cf183a9f0fc676164d3bf776595e8ef3a21a992
opadotun-taiwo <opadotunoluwaseun@yahoo.com>
2025-08-13 05:01:00 -0700

    dbt bigquery sync

---
+++ macros/generate_scheme.sql
@@ -0,0 +1,7 @@
+{% set models_to_generate = codegen.get_models(directory='staging', prefix='') %}
+{{ codegen.generate_model_yaml(
+    model_names = models_to_generate
+) }}
+
+
+-- dbt run-operation generate_model_yaml --args '{"model_names": ["dm_montly_zone_revenue", "dim_zone", "fact_trips"]}'
